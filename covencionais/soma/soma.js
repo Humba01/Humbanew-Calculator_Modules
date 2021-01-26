9a4fe222012ca58c3da30fea01c4f75e4faf6eb4
Humberto Bravo <humbandroid@gmail.com>
2021-01-26 15:49:27 -0300

    Adicionando Sistema de Segurança Anti-Travamento de calcúlos pt.2

--- covencionais/soma/soma.js
+++ covencionais/soma/soma.js
@@ -1,8 +1,9 @@
 var valores = ["2", "3.0"];
-var expressaoIntString = /(\d+)?/ig;
-var expressaoFloatString = /(\d+)?\.(\d+)?/ig;
-console.log(new RegExp(expressaoIntString, "gi").test(valores[0]));
-console.log(new RegExp(expressaoFloatString, "gi").test(valores[0]));
-console.log(new RegExp(expressaoIntString, "gi").test(valores[1]));
-console.log(new RegExp(expressaoFloatString, "gi").test(valores[1]));
+var verificaErroTipoUm = /(\d+[a-zA-Z]+\d+)/gi;
+var verificaErroTipoDois = /(\d+[a-zA-Z]+\d+[a-zA-Z]+)/gi;
+function testaEntradaUsuario(regex, tagsParaRegex, alvoParaAnalise) {
+    if (regex === void 0) { regex = RegExp || ""; }
+    if (tagsParaRegex === void 0) { tagsParaRegex = ""; }
+    if (alvoParaAnalise === void 0) { alvoParaAnalise = ""; }
+}
 var totalDaConta = 0;
