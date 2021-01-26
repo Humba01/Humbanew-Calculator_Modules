66443a3f9fc79a06a62996328846cb66068f53ca
Humberto Bravo <humbandroid@gmail.com>
2021-01-26 16:06:31 -0300

    Adicionando Sistema de Segurança Anti-Travamento de calcúlos pt.3

--- covencionais/soma/soma.js
+++ covencionais/soma/soma.js
@@ -1,6 +1,12 @@
 var valores = ["2", "3.0"];
-var verificaErroTipoUm = /(\d+[a-zA-Z]+\d+)/gi;
-var verificaErroTipoDois = /(\d+[a-zA-Z]+\d+[a-zA-Z]+)/gi;
+var verificaErroTipoUm = /(\d+[a-zA-Z]+)/gi;
+var verificaErroTipoDois = /(\d+[a-zA-Z]+\d+)/gi;
+var verificaErroTipoTres = /(\d+[a-zA-Z]+\d+[a-zA-Z]+)/gi;
+var verificaErroTipoQuatro = /([a-zA-Z]+\d+[a-zA-Z]+\d+[a-zA-Z]+)/gi;
+var verificaErroTipoCinco = /(\d+[a-zA-Z]+)(\.)(\d+[a-zA-Z]+)/gi;
+var verificaErroTipoSeis = /(\d+[a-zA-Z]+\d+)(\.)(\d+[a-zA-Z]+\d+)/gi;
+var verificaErroTipoSete = /(\d+[a-zA-Z]+\d+[a-zA-Z]+)(\.)(\d+[a-zA-Z]+\d+[a-zA-Z]+)/gi;
+var verificaErroTipoOito = /([a-zA-Z]+\d+[a-zA-Z]+\d+[a-zA-Z]+)(\.)([a-zA-Z]+\d+[a-zA-Z]+\d+[a-zA-Z]+)/gi;
 function testaEntradaUsuario(regex, tagsParaRegex, alvoParaAnalise) {
     if (regex === void 0) { regex = RegExp || ""; }
     if (tagsParaRegex === void 0) { tagsParaRegex = ""; }
--- covencionais/soma/soma.ts
+++ covencionais/soma/soma.ts
@@ -1,8 +1,13 @@
 const valores: string[] = ["2", "3.0"];
 
-const verificaErroTipoUm: RegExp = /(\d+[a-zA-Z]+\d+)/gi;
-const verificaErroTipoDois: RegExp = /(\d+[a-zA-Z]+\d+[a-zA-Z]+)/gi;
-
+const verificaErroTipoUm: RegExp = /(\d+[a-zA-Z]+)/gi;
+const verificaErroTipoDois: RegExp = /(\d+[a-zA-Z]+\d+)/gi;
+const verificaErroTipoTres: RegExp = /(\d+[a-zA-Z]+\d+[a-zA-Z]+)/gi;
+const verificaErroTipoQuatro: RegExp = /([a-zA-Z]+\d+[a-zA-Z]+\d+[a-zA-Z]+)/gi;
+const verificaErroTipoCinco: RegExp = /(\d+[a-zA-Z]+)(\.)(\d+[a-zA-Z]+)/gi;
+const verificaErroTipoSeis: RegExp = /(\d+[a-zA-Z]+\d+)(\.)(\d+[a-zA-Z]+\d+)/gi;
+const verificaErroTipoSete: RegExp = /(\d+[a-zA-Z]+\d+[a-zA-Z]+)(\.)(\d+[a-zA-Z]+\d+[a-zA-Z]+)/gi;
+const verificaErroTipoOito: RegExp = /([a-zA-Z]+\d+[a-zA-Z]+\d+[a-zA-Z]+)(\.)([a-zA-Z]+\d+[a-zA-Z]+\d+[a-zA-Z]+)/gi;
 
 
 function testaEntradaUsuario(regex=RegExp||"", tagsParaRegex="", alvoParaAnalise="") {
