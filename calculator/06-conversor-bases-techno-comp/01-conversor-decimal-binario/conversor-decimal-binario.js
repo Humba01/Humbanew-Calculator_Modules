11b163b17ef087b7362fbb35335fb558f636b846
Humberto Bravo <humbandroid@gmail.com>
2021-02-07 01:49:42 -0300

    Adicionando alterações no módulo de conversão Decimal-Binário PT.2

--- calculator/06-conversor-bases-techno-comp/01-conversor-decimal-binario/conversor-decimal-binario.js
+++ calculator/06-conversor-bases-techno-comp/01-conversor-decimal-binario/conversor-decimal-binario.js
@@ -1,4 +1,4 @@
-var valores = ["50", "44", "100", "-500"];
+var valores = ["50", "44", "100", "300.5"];
 var valorTotalPConv = 0;
 var numeroPontoFlutuante;
 for (var y = 0; y < valores.length; y++) {
@@ -10,66 +10,62 @@ for (var y = 0; y < valores.length; y++) {
         console.log("\nEtapa 1 => Valor Total: ", valorTotalPConv);
     }
 }
-numeroPontoFlutuante = 0;
-if (numeroPontoFlutuante == 0) {
-    var numeroBinarioConstructor = [];
-    var numeroBinarioSinal = "";
-    var numeroBinarioResultado = "";
-    if (valorTotalPConv < 0) {
-        numeroBinarioSinal = numeroBinarioSinal + "1";
-        console.warn("\tNúmero é negativo.", numeroBinarioSinal);
-        valorTotalPConv = valorTotalPConv * -1;
-    }
-    if (valorTotalPConv > 0) {
-        numeroBinarioSinal = numeroBinarioSinal + "0";
-        console.warn("\tNúmero é positivo.", numeroBinarioSinal);
-    }
-    while (valorTotalPConv >= 2) {
-        valorTotalPConv = Math.floor(valorTotalPConv) / 2;
-        console.log("\nEtapa 2 => Dividindo para montar o número binário: ", valorTotalPConv);
-        if (Number.isInteger(valorTotalPConv) == true) {
-            numeroBinarioConstructor.push("0");
-            valorTotalPConv = Math.floor(valorTotalPConv);
-        }
-        if (Number.isInteger(valorTotalPConv) == false) {
-            numeroBinarioConstructor.push("1");
-            valorTotalPConv = Math.floor(valorTotalPConv);
-        }
-        if (valorTotalPConv == 1) {
-            numeroBinarioConstructor.push("1");
-            valorTotalPConv = Math.floor(valorTotalPConv);
-        }
-        if (valorTotalPConv == 0) {
-            numeroBinarioConstructor.push("0");
-            valorTotalPConv = Math.floor(valorTotalPConv);
-        }
-        console.log("\nEtapa 3 => Construção do número binário: ", numeroBinarioConstructor);
-    }
-    numeroBinarioConstructor = numeroBinarioConstructor.reverse();
-    console.log("\nEtapa 4 => Inversão do construtor para a formação do resultado final: ", numeroBinarioConstructor);
-    for (var u = 0; u < numeroBinarioConstructor.length; u++) {
-        numeroBinarioResultado = numeroBinarioResultado + numeroBinarioConstructor[u];
-        console.log("\nEtapa 5 => Montando resultado final: ", numeroBinarioResultado);
-    }
-    var numeroBinarioRCSinal = numeroBinarioSinal + numeroBinarioResultado;
-    console.log("\nEtapa 6 => Adicionando o sinal ao resultado: ", numeroBinarioRCSinal);
-    console.log("\n\tEtapa 7 => Resultado Final: ", numeroBinarioRCSinal);
+var numeroBinarioConstructor = [];
+var numeroBinarioSinal = "";
+var numeroBinarioResultado = "";
+if (valorTotalPConv < 0) {
+    numeroBinarioSinal = numeroBinarioSinal + "1";
+    console.warn("\tNúmero é negativo.", numeroBinarioSinal);
+    valorTotalPConv = valorTotalPConv * -1;
 }
-if (numeroPontoFlutuante == 1) {
-    // console.error("Função ainda em desenvolvimento!");
-    var numeroBinarioConstrutorInteiros = [];
-    var numeroBinarioConstrutorDecimais = [];
-    var pegaParteDecimal = /\.(\d+)$/gi;
-    var pegaParteInteira = /^(\d+)/gi;
-    while (valorTotalPConv >= 2.0) {
-        if (valorTotalPConv < 0.0) {
-            numeroBinarioConstrutorInteiros.push("1");
-        }
-        if (valorTotalPConv > 0.0) {
-            numeroBinarioConstrutorInteiros.push("0");
-        }
-    }
-    var $1 = valorTotalPConv.toString().match(pegaParteDecimal);
-    var $2 = valorTotalPConv.toString().match(pegaParteInteira);
-    console.log($1, $2);
+if (valorTotalPConv > 0) {
+    numeroBinarioSinal = numeroBinarioSinal + "0";
+    console.warn("\tNúmero é positivo.", numeroBinarioSinal);
 }
+while (valorTotalPConv >= 2) {
+    valorTotalPConv = Math.floor(valorTotalPConv) / 2;
+    console.log("\nEtapa 2 => Dividindo para montar o número binário: ", valorTotalPConv);
+    if (Number.isInteger(valorTotalPConv) == true) {
+        numeroBinarioConstructor.push("0");
+        valorTotalPConv = Math.floor(valorTotalPConv);
+    }
+    if (Number.isInteger(valorTotalPConv) == false) {
+        numeroBinarioConstructor.push("1");
+        valorTotalPConv = Math.floor(valorTotalPConv);
+    }
+    if (valorTotalPConv == 1) {
+        numeroBinarioConstructor.push("1");
+        valorTotalPConv = Math.floor(valorTotalPConv);
+    }
+    if (valorTotalPConv == 0) {
+        numeroBinarioConstructor.push("0");
+        valorTotalPConv = Math.floor(valorTotalPConv);
+    }
+    console.log("\nEtapa 3 => Construção do número binário: ", numeroBinarioConstructor);
+}
+numeroBinarioConstructor = numeroBinarioConstructor.reverse();
+console.log("\nEtapa 4 => Inversão do construtor para a formação do resultado final: ", numeroBinarioConstructor);
+for (var u = 0; u < numeroBinarioConstructor.length; u++) {
+    numeroBinarioResultado = numeroBinarioResultado + numeroBinarioConstructor[u];
+    console.log("\nEtapa 5 => Montando resultado final: ", numeroBinarioResultado);
+}
+console.log("\n\tEtapa 6 => Resultado Final: ", numeroBinarioSinal, numeroBinarioResultado);
+// if(numeroPontoFlutuante == 1) {
+//   // console.error("Função ainda em desenvolvimento!");
+//   var numeroBinarioConstrutorInteiros: string[] = [];
+//   var numeroBinarioConstrutorDecimais: string[] = [];
+//   let pegaParteDecimal: RegExp = /\.(\d+)$/gi;
+//   let pegaParteInteira: RegExp = /^(\d+)/gi;
+//   while(valorTotalPConv >= 2.0) {
+//     if(valorTotalPConv < 0.0) {
+//       numeroBinarioConstrutorInteiros.push("1");
+//     }
+//     if(valorTotalPConv > 0.0) {
+//       numeroBinarioConstrutorInteiros.push("0");
+//     }
+//   }
+//   let w1 = valorTotalPConv.toString().match(pegaParteDecimal);
+//   let w2 = valorTotalPConv.toString().match(pegaParteInteira);
+//   // $1 = Math.floor($1) / 2;
+//   console.log(w1, w2);
+// }
--- calculator/06-conversor-bases-techno-comp/01-conversor-decimal-binario/conversor-decimal-binario.ts
+++ calculator/06-conversor-bases-techno-comp/01-conversor-decimal-binario/conversor-decimal-binario.ts
@@ -1,4 +1,4 @@
-var valores: string[] = ["50", "44", "100", "-500"];
+var valores: string[] = ["50", "44", "100", "300"];
 
 var valorTotalPConv = 0;
 
@@ -13,91 +13,85 @@ for(let y = 0; y < valores.length; y++) {
   }
 }
 
-numeroPontoFlutuante = 0;
+var numeroBinarioConstructor: string[] = [];
+var numeroBinarioSinal: string = "";
+var numeroBinarioResultado: string = "";
 
-if(numeroPontoFlutuante == 0) {
-
-  var numeroBinarioConstructor: string[] = [];
-  var numeroBinarioSinal: string = "";
-  var numeroBinarioResultado: string = "";
-
-  if(valorTotalPConv < 0) {
-    numeroBinarioSinal = numeroBinarioSinal + "1";
-    console.warn("\tNúmero é negativo.", numeroBinarioSinal);
-    valorTotalPConv = valorTotalPConv * -1;
-  }
-  
-  if(valorTotalPConv > 0) {
-    numeroBinarioSinal = numeroBinarioSinal + "0";
-    console.warn("\tNúmero é positivo.", numeroBinarioSinal);
-  }
-  
-  while(valorTotalPConv >= 2) {
-  
-    valorTotalPConv = Math.floor(valorTotalPConv) / 2;
-    console.log("\nEtapa 2 => Dividindo para montar o número binário: ", valorTotalPConv);
-  
-    if(Number.isInteger(valorTotalPConv) == true) {
-      numeroBinarioConstructor.push("0");
-      valorTotalPConv = Math.floor(valorTotalPConv)
-    }
-  
-    if(Number.isInteger(valorTotalPConv) == false) {
-      numeroBinarioConstructor.push("1");
-      valorTotalPConv = Math.floor(valorTotalPConv)
-    }
-  
-    if(valorTotalPConv == 1) {
-      numeroBinarioConstructor.push("1");
-      valorTotalPConv = Math.floor(valorTotalPConv)
-    }
-  
-    if(valorTotalPConv == 0) {
-      numeroBinarioConstructor.push("0");
-      valorTotalPConv = Math.floor(valorTotalPConv)
-    }
-  
-    console.log("\nEtapa 3 => Construção do número binário: ", numeroBinarioConstructor);
-  }
-  
-  numeroBinarioConstructor = numeroBinarioConstructor.reverse();
-  console.log("\nEtapa 4 => Inversão do construtor para a formação do resultado final: ", numeroBinarioConstructor);
-  
-  for(let u = 0; u < numeroBinarioConstructor.length; u++) {
-    numeroBinarioResultado = numeroBinarioResultado + numeroBinarioConstructor[u];
-    console.log("\nEtapa 5 => Montando resultado final: ", numeroBinarioResultado);
-  }
-
-  let numeroBinarioRCSinal = numeroBinarioSinal + numeroBinarioResultado;
-  console.log("\nEtapa 6 => Adicionando o sinal ao resultado: ", numeroBinarioRCSinal);
-  
-  console.log("\n\tEtapa 7 => Resultado Final: ", numeroBinarioRCSinal);
+if(valorTotalPConv < 0) {
+  numeroBinarioSinal = numeroBinarioSinal + "1";
+  console.warn("\tNúmero é negativo.", numeroBinarioSinal);
+  valorTotalPConv = valorTotalPConv * -1;
 }
 
-if(numeroPontoFlutuante == 1) {
-  // console.error("Função ainda em desenvolvimento!");
+if(valorTotalPConv > 0) {
+  numeroBinarioSinal = numeroBinarioSinal + "0";
+  console.warn("\tNúmero é positivo.", numeroBinarioSinal);
+}
 
-  var numeroBinarioConstrutorInteiros: string[] = [];
-  var numeroBinarioConstrutorDecimais: string[] = [];
+while(valorTotalPConv >= 2) {
 
-  let pegaParteDecimal: RegExp = /\.(\d+)$/gi;
-  let pegaParteInteira: RegExp = /^(\d+)/gi;
+  valorTotalPConv = Math.floor(valorTotalPConv) / 2;
+  console.log("\nEtapa 2 => Dividindo para montar o número binário: ", valorTotalPConv);
 
-  while(valorTotalPConv >= 2.0) {
+  if(Number.isInteger(valorTotalPConv) == true) {
+    numeroBinarioConstructor.push("0");
+    valorTotalPConv = Math.floor(valorTotalPConv)
+  }
+
+  if(Number.isInteger(valorTotalPConv) == false) {
+    numeroBinarioConstructor.push("1");
+    valorTotalPConv = Math.floor(valorTotalPConv)
+  }
+
+  if(valorTotalPConv == 1) {
+    numeroBinarioConstructor.push("1");
+    valorTotalPConv = Math.floor(valorTotalPConv)
+  }
+
+  if(valorTotalPConv == 0) {
+    numeroBinarioConstructor.push("0");
+    valorTotalPConv = Math.floor(valorTotalPConv)
+  }
+
+  console.log("\nEtapa 3 => Construção do número binário: ", numeroBinarioConstructor);
+}
+
+numeroBinarioConstructor = numeroBinarioConstructor.reverse();
+console.log("\nEtapa 4 => Inversão do construtor para a formação do resultado final: ", numeroBinarioConstructor);
+
+for(let u = 0; u < numeroBinarioConstructor.length; u++) {
+  numeroBinarioResultado = numeroBinarioResultado + numeroBinarioConstructor[u];
+  console.log("\nEtapa 5 => Montando resultado final: ", numeroBinarioResultado);
+}
+
+console.log("\n\tEtapa 6 => Resultado Final: ", numeroBinarioSinal, numeroBinarioResultado);
+
+// if(numeroPontoFlutuante == 1) {
+//   // console.error("Função ainda em desenvolvimento!");
+
+//   var numeroBinarioConstrutorInteiros: string[] = [];
+//   var numeroBinarioConstrutorDecimais: string[] = [];
+
+//   let pegaParteDecimal: RegExp = /\.(\d+)$/gi;
+//   let pegaParteInteira: RegExp = /^(\d+)/gi;
+
+//   while(valorTotalPConv >= 2.0) {
     
-    if(valorTotalPConv < 0.0) {
-      numeroBinarioConstrutorInteiros.push("1");
-    }
+//     if(valorTotalPConv < 0.0) {
+//       numeroBinarioConstrutorInteiros.push("1");
+//     }
 
-    if(valorTotalPConv > 0.0) {
-      numeroBinarioConstrutorInteiros.push("0");
-    }
+//     if(valorTotalPConv > 0.0) {
+//       numeroBinarioConstrutorInteiros.push("0");
+//     }
 
-  }
+//   }
 
-  let $1 = valorTotalPConv.toString().match(pegaParteDecimal);
-  let $2 = valorTotalPConv.toString().match(pegaParteInteira);
+//   let w1 = valorTotalPConv.toString().match(pegaParteDecimal);
+//   let w2 = valorTotalPConv.toString().match(pegaParteInteira);
+
+//   // $1 = Math.floor($1) / 2;
   
-  console.log($1, $2);
+//   console.log(w1, w2);
 
-}
+// }
